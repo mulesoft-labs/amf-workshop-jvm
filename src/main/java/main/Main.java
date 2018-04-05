38a605a6a51fa736133110c6678783c1b0ca55d1
Hernan Najles <hernan.najles@mulesoft.com>
2018-04-05 16:18:09 -0300

    Initial commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+    
+    public static void main(String[] args) {
+    
+    }
+    
+    final static String filePath = "/amf-workshop-jvm/src/main/resources";
+}
