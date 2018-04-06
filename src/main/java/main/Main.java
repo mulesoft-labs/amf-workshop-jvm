6bb137f164501e67c2d15f220b9aef7bae04af2c
Ignacio Manzano <ignacio.manzano@gmail.com>
2018-04-06 11:50:51 -0300

    from raml to oas

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
 import amf.client.AMF;
+import amf.client.model.document.BaseUnit;
+import amf.client.model.document.Document;
+import amf.client.model.domain.DomainElement;
+import amf.client.parse.RamlParser;
 
 import java.util.concurrent.ExecutionException;
 
@@ -9,6 +13,19 @@ public class Main {
     public static void main(String[] args) {
         try {
             AMF.init().get();
+
+            final RamlParser ramlParser = AMF.ramlParser();
+
+            final BaseUnit baseUnit = ramlParser.parseFileAsync(filePath).get();
+
+            final Document document = (Document) baseUnit;
+
+            final DomainElement encodes = document.encodes();
+
+            System.out.println(document.platform());
+
+            final String s = AMF.oas20Generator().generateString(baseUnit).get();
+            s.length();
 
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
