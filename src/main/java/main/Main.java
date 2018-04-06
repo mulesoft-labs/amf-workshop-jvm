5c9df6463eb0e288338fba23eae03e32dfa7c440
Ignacio Manzano <ignacio.manzano@gmail.com>
2018-04-06 12:02:32 -0300

    mutate values

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,9 @@ import amf.client.AMF;
 import amf.client.model.document.BaseUnit;
 import amf.client.model.document.Document;
 import amf.client.model.domain.DomainElement;
+import amf.client.model.domain.ScalarShape;
+import amf.client.model.domain.Shape;
+import amf.client.model.domain.WebApi;
 import amf.client.parse.RamlParser;
 import amf.client.validate.ValidationReport;
 import amf.client.validate.ValidationResult;
@@ -24,7 +27,7 @@ public class Main {
 
             final Document document = (Document) baseUnit;
 
-
+            WebApi webApi = (WebApi)document.encodes();
             final ValidationReport validationReport = AMF.validate(baseUnit, ProfileNames.RAML(), ProfileNames.RAML()).get();
             System.out.println(validationReport.conforms());
             final List<ValidationResult> results = validationReport.results();
@@ -32,6 +35,15 @@ public class Main {
                 System.out.println(result);
             }
 
+            final ScalarShape schema = (ScalarShape)webApi.endPoints().get(0).operations().get(0).request().payloads().get(0).schema();
+            schema.withMaxLength(-1);
+
+            final ValidationReport validationReport2 = AMF.validate(baseUnit, ProfileNames.RAML(), ProfileNames.RAML()).get();
+            System.out.println(validationReport2.conforms());
+            final List<ValidationResult> results2 = validationReport.results();
+            for (ValidationResult result : results2) {
+                System.out.println(result);
+            }
 
             final DomainElement encodes = document.encodes();
 
